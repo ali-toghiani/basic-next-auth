22c81664804ef6677f37ec82a5f2a3d2153f6e50
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-08-06 20:11:29 +0330

    feat(not-found.tsx): add return to dashboard button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,23 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import classes from "./page.module.scss"
+import Button from '@/components/Button/Button';
+
 export default function NotFound() {
+
+  const router = useRouter();
+
+  function handleGoToDashboard() {
+    router.push('/dashboard');
+  }
+  
   return (
-    <div>
+    <main className={classes['not-found-layout']}>
       <h1>Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
-    </div>
+      <Button type="button" onClick={handleGoToDashboard}>Return to Dashboard</Button>
+    </main>
   );
 }
