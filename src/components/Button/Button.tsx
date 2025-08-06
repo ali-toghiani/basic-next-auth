589c6ee723dc5e2196e73fca9a3626f399c15eaf
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-08-06 17:12:54 +0330

    feat(Button.tsx): add isLoading prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,15 @@
 import classes from './Button.module.scss';
 import { ButtonProps } from '@/types/Button';
 
-export default function Button({children, className, type, ...rest}: ButtonProps){
+export default function Button({children, className, type, isLoading, ...rest}: ButtonProps){
   return (
-    <button className={`${classes['button']} ${className}`} type={type || 'button'} {...rest}>
-      {children}
+    <button 
+      className={`${classes.button} ${className || ''}`} 
+      type={type || 'button'} 
+      disabled={isLoading || rest.disabled} 
+      {...rest}
+    >
+      {isLoading ? <span className={classes.spinner}></span> : children}
     </button>
   );
 }
