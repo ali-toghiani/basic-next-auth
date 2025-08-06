2c7dea2ac0773c2c7bee437ac6aefe22c59f3bac
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-08-06 17:20:56 +0330

    init(types): create User types for UserContext

---
+++ src/types/User.ts
@@ -0,0 +1,80 @@
+export interface UserContextType {
+  user: User | null;
+  loading: boolean;
+  error: string | null;
+  setUser: (data: User | null) => void;
+  setLoading: (loading: boolean) => void;
+  setError: (error: string | null) => void;
+}
+export interface UserResponse {
+  results: User[];
+  info: Info;
+}
+interface Info {
+  seed: string;
+  results: number;
+  page: number;
+  version: string;
+}
+export interface User {
+  gender: string;
+  name: Name;
+  location: Location;
+  email: string;
+  login: Login;
+  dob: Dob;
+  registered: Dob;
+  phone: string;
+  cell: string;
+  id: Id;
+  picture: Picture;
+  nat: string;
+}
+interface Picture {
+  large: string;
+  medium: string;
+  thumbnail: string;
+}
+interface Id {
+  name: string;
+  value: string;
+}
+interface Dob {
+  date: string;
+  age: number;
+}
+interface Login {
+  uuid: string;
+  username: string;
+  password: string;
+  salt: string;
+  md5: string;
+  sha1: string;
+  sha256: string;
+}
+interface Location {
+  street: Street;
+  city: string;
+  state: string;
+  country: string;
+  postcode: number;
+  coordinates: Coordinates;
+  timezone: Timezone;
+}
+interface Timezone {
+  offset: string;
+  description: string;
+}
+interface Coordinates {
+  latitude: string;
+  longitude: string;
+}
+interface Street {
+  number: number;
+  name: string;
+}
+interface Name {
+  title: string;
+  first: string;
+  last: string;
+}
